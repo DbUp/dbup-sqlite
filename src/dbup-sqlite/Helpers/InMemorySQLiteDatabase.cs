025c940a173438e98e9ec74c14c135008ffd0780
Robert Wagner <robert@wagner.id.au>
2024-07-29 17:09:19 +1000

    Removed PRAGMA because it doesn't work in this version

--- src/dbup-sqlite/Helpers/InMemorySQLiteDatabase.cs
+++ src/dbup-sqlite/Helpers/InMemorySQLiteDatabase.cs
@@ -23,8 +23,7 @@ namespace DbUp.SQLite.Helpers
             {
                 DataSource = ":memory:",
                 DefaultTimeout = 5,
-                Mode = SqliteOpenMode.Memory,
-                ConnectionString = "PRAGMA encoding='UTF-16'; PRAGMA journal_mode='MEMORY';"
+                Mode = SqliteOpenMode.Memory
             };
             ConnectionString = connectionStringBuilder.ToString();
 
